231e6247e9a2314bee3e711937e9cf58fc8204e6
flos2soon-arch <flos2soon@gmail.com>
2026-04-20 20:21:11 +0800

    letterData.js
    Updated a sentence to improve clarity and expression.

--- src/data/letterData.js
+++ src/data/letterData.js
@@ -8,7 +8,7 @@ export const letterData = {
      "You probably don't know, but there are moments that feel “marked” to me. I don't want to forget them. To me, they're like something precious.",
     "I wanted to keep those normal or sweet moments, the way we talked, the things you said, your tone… and those moments that I keep replaying in my head. I think about our late calls, the way we used to be clingy with each other, our little inside jokes. I want to keep every memories frequency in my head forever. It's also the inspiration that i made this web.",
     "But sometimes those same “frequencies” also hurt. It could be something you said casually, or just a normal conversation, but somehow they turn into things that replay over and over in my mind. I can't completely erase them, and I don't want to pretend they don't matter. I really want us to fix them together.",
-    "I'm sorry for all the arguments lately. I don't want things to be like this. I just feel like our relationship shouldn't be this way. We deserve something better. I really do love you.",
+    "I'm sorry for all the arguments lately. I don't want things to be like this. I just feel our relationship should be better and better. I really do love you.",
     "This is my first time making a website like this. I don't know anything about coding. I spent a lot of time and money on it, and it still didn't turn out exactly how I imagined. There are many things I wanted to make but I couldn't. But I really put so much thought and effort into it. I hope one day i can make it better and I hope you like it.",
     "",
     "Happy birthday my baby.",
